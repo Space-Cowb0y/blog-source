792b86e2b5fb019d524911a4a742b568a90ef6bc
Space_Cowb0y <brunohenrique.oliveiracarvalho@gmail.com>
2024-02-05 02:43:59 -0300

    teste1

--- publish.py
+++ publish.py
@@ -1,8 +1,8 @@
 import os
 
-os.system("cd C:\Users\Pichau\Documents\Projetos\\blog-source")
+os.system("cd C:\\Users\\Pichau\\Documents\\Projetos\\blog-source")
 os.system("git add *")
 os.system("git commit -m" + input("Commit message: "))
-os.system("git push")
 os.system("cd /public/ && git add * && git commit -m" + input("Commit message: "))
-os.system("git push")
+os.system("cd ..")
+os.system("git push --recurse-submodules=on-demand")
