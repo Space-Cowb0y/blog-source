e75c6d1dccc241803273f9804106978b0a3bcdca
Space_Cowb0y <brunohenrique.oliveiracarvalho@gmail.com>
2024-02-05 02:41:18 -0300

    correçao

--- publish.py
+++ publish.py
@@ -1,1 +1,8 @@
-import so
+import os
+
+os.system("cd C:\Users\Pichau\Documents\Projetos\\blog-source")
+os.system("git add *")
+os.system("git commit -m" + input("Commit message: "))
+os.system("git push")
+os.system("cd /public/ && git add * && git commit -m" + input("Commit message: "))
+os.system("git push")
